56736a5326d1ca71f6a599453e52f979fa46cc4b
florence-njeri <flonsie17@gmail.com>
2019-05-09 22:10:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.android.diceroller
+
+import android.annotation.SuppressLint
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import java.util.*
+
+@Suppress("UNREACHABLE_CODE")
+class MainActivity : AppCompatActivity() {
+    lateinit var  diceImage : ImageView
+    @SuppressLint("ShowToast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button =findViewById(R.id.roll_button)
+
+
+        rollButton.text="Let's roll!!"
+        rollButton.setOnClickListener {
+rollDice()
+        }
+        diceImage =findViewById(R.id.dice_image_view)
+    }
+
+    private fun rollDice() {
+
+        val randomInt=Random().nextInt(6)+1
+        val drawableResource=when(randomInt){
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else->R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+
+    }
+
+}
